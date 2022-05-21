10c4737ab21c3e80b7f1b75439546a804ed8862d
Diego Dmitry <diegodmitry@hotmail.com>
2022-05-21 10:28:59 -0300

    feat: added user.js file into models directory. Req 2

---
+++ src/database/models/user.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    displayName: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.STRING
+    },
+  }, {
+    timestamps: false,
+    tableName: 'Users',
+  });
+  // Se entendi correto de acordo com o site, https://www.lucidchart.com/pages/pt/o-que-e-diagrama-entidade-relacionamento,
+  // o relacionamento entre a tabela Users e BlogPosts é de 1:N.
+  // Então acho que é necessário, associar a tabela Users com a tabela BlogPosts
+  // userId é foreingKey dentro de BlogPosts
+  // Source: https://sequelize.org/docs/v6/core-concepts/assocs/
+  // BlogPost será criada no requesito à frente
+  Users.associate = (models) => {
+    Users.hasMany(models.BlogPosts, { foreignKey: 'userId' });
+  };
+  return Users;
+}; 
