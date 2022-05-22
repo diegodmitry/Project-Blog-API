564e1f1b49733a6a66994b817cb19ce8de315d79
Diego Dmitry <diegodmitry@hotmail.com>
2022-05-22 20:10:47 -0300

    feat: added createUser middleware. Req 4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,10 +5,27 @@ const userLogin = async (req, res) => {
     const { email, password } = req.body;
   
     const isLoginOk = await userServices.loginValidation(email, password);
+
     if (isLoginOk.token) return res.status(isLoginOk.status).json({ token: isLoginOk.token });
+
     return res.status(isLoginOk.status).json({ message: isLoginOk.message });
   } catch (error) {
     console.log(error.message);
   }
 };
-module.exports = { userLogin };
+
+const createUser = async (req, res) => {
+  try {
+    const { displayName, email, password, image } = req.body;
+
+    const data = await userServices
+      .createUserValidation(displayName, email, password, image);
+
+    if (data.token) return res.status(201).json({ token: data.token });
+
+    return res.status(data.status).json({ message: data.message });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+module.exports = { userLogin, createUser };
