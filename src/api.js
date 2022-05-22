3e18a210a78c21094c4cdc995dd7482b4e622727
Diego Dmitry <diegodmitry@hotmail.com>
2022-05-22 20:09:17 -0300

    feat: added /user route with post method

--- src/api.js
+++ src/api.js
@@ -8,6 +8,8 @@ app.use(express.json());
 
 app.post('/login', userController.userLogin);
 
+app.post('/user', userController.createUser);
+
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
