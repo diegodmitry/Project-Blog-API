dc56f52758064d52e98ce292face92616ea48ab9
Diego Dmitry <diegodmitry@hotmail.com>
2022-05-22 15:10:48 -0300

    feat: add login route. Req3

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-// ...
+const userController = require('./controllers/userController');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.post('/login', userController.userLogin);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
