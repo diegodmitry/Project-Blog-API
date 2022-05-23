71b3701c168684a471e89efe9e71792791853555
Diego Dmitry <diegodmitry@hotmail.com>
2022-05-22 21:30:50 -0300

    feat: added /user route, added auth middleware into route and getAllUsers. Req 5

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const userController = require('./controllers/userController');
+const { auth } = require('./middlewares/auth');
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.post('/login', userController.userLogin);
 
 app.post('/user', userController.createUser);
+
+app.get('/user', auth, userController.getAllUsers);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
