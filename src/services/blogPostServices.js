976e7916c6eeb723cc904269f6666d00f30b7016
Diego Dmitry <diegodmitry@hotmail.com>
2022-05-24 17:45:15 -0300

    feat: added 2 hasCategoryOnDb, fieldPostValidation, addBlogPost, getAllPosts. Reqs 12, 13.

--- src/services/blogPostServices.js
+++ src/services/blogPostServices.js
@@ -1,20 +1,73 @@
-
 const { BlogPost, Category, User } = require('../database/models');
 
-const fieldPostValidation = async (title, content, categoryIds, id) => {
-// console.log("🚀 ~ file: blogPostServices.js ~ line 9 ~ fieldPostValidation ~ categoryIds", categoryIds)
-  if (!title || !content || !categoryIds) return { status: 400, message: 'Some required fields are missing' };
-  if (categoryIds.length < 1) return { status: 400, message: '"categoryIds" not found' };
-  
-
-
+// Req 12
+const hasCategoryOnDb = async (categoryIds) => {
+  // Get all categories from DB
+  const allCategories = await Category.findAll();
+  const idListFromDB = allCategories.map((cat) => cat.dataValues.id);
+  categoryIds.forEach((id) => {
+    // comparison returns true or false
+    const hasInclude = idListFromDB.includes(id);
+    if (!hasInclude) throw new Error('Category is not found');
+  });
 };
 
-const addBlogPost = async (title, content, categoryIds, id) => {
-  const data = await fieldPostValidation(title, content, categoryIds, id);
-  // console.log("🚀 ~ file: blogPostServices.js ~ line 33 ~ addBlogPost ~ data", data)
+// Req 12
+const fieldPostValidation = async (body, id) => {
+  if (!body.title || !body.content) {
+    return { status: 400, message: 'Some required fields are missing' };
+  }
+  if (!body.categoryIds || body.categoryIds.length < 1) {
+    return { status: 400, message: '"categoryIds" not found' };
+  }
+  // check if there is category
+  await hasCategoryOnDb(body.categoryIds);
+
+  const createdPost = await BlogPost.create({
+    title: body.title,
+    content: body.content,
+    userId: id,
+    updated: Date.now(),
+    published: Date.now(),
+  });
+
+  // create row into PostCategories
+  // Source: https://sequelize.org/docs/v6/core-concepts/assocs/#foobelongstomanybar--through-baz-
+  await createdPost.addCategories(body.categoryIds);
+
+  return { status: 201, createdPost };
+};
+
+// Req 12
+const addBlogPost = async (body, id) => {
+  const data = await fieldPostValidation(body, id);
   return data;
 };
 
+// Req 13
+const getAllPosts = async () => {
+  const allPosts = await BlogPost.findAll({
+    include: [{
+      model: User,
+      as: 'user',
+      attributes: { exclude: ['password'] },
+    },
+    {
+      model: Category,
+      as: 'categories',
+      through: { attributes: [] },
+    }],
+  });
+  return allPosts;
+};
 
-module.exports = { addBlogPost };
+// Req 14
+// const getById = async (id) => {
+//   const post = await BlogPost.findByPk(id, { include: [
+//     { model: User, as: 'user', attributes: { exclude: 'password' } },
+//     { model: Category, as: 'categories', through: { attributes: [] } }] });
+//   console.log("🚀 ~ file: blogPostServices.js ~ line 73 ~ getById ~ post", post)
+//   // return post;
+// };
+
+module.exports = { addBlogPost, getAllPosts };
