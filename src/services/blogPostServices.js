be4a2007eafe0258618944d1a3d7a2cc9a7d1a2f
Diego Dmitry <diegodmitry@hotmail.com>
2022-05-25 19:17:17 -0300

    feat: added postSearch function. Req 18

--- src/services/blogPostServices.js
+++ src/services/blogPostServices.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { BlogPost, Category, User } = require('../database/models');
 
 // Req 12
@@ -112,4 +113,22 @@ const deleteBlogPost = async (id, userId) => {
   return {};
 };
 
-module.exports = { addBlogPost, getAllPosts, getById, updateBlogPost, deleteBlogPost };
+// Req 18
+// Source: https://sequelize.org/docs/v6/core-concepts/model-querying-basics/
+const postSearch = async (q) => {
+  const searchedPosts = await BlogPost.findAll({
+    where: {
+      [Op.or]: [
+        { title: { [Op.like]: `%${q}%` } },
+        { content: { [Op.like]: `%${q}%` } },
+      ],
+    },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+  return searchedPosts;
+};
+
+module.exports = { addBlogPost, getAllPosts, getById, updateBlogPost, deleteBlogPost, postSearch };
